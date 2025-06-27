d130e3f2494b814da3095b9be4e8634d7faf8d09
leoRV14 <leandro.rain@inacapmail.cl>
2025-06-27 00:09:20 -0400

    Create App.py

---
+++ App.py
@@ -0,0 +1,12 @@
+# Docker puerto 8888
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "¡Hola desde Docker en puerto 8888!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8888)
